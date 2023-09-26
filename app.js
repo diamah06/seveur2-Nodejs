863f1108aebd5188f9bffe84556197b103097cb8
diamah06 <tabalmahdia@yahoo.fr>
2023-09-26 12:58:16 +0200

    second

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app implements a router plus middlewares (logger, json, url encoded, cookie parser, etc.)
 app.use('/', indexRouter);
-
 
 module.exports = app;
